ab86d6c67921eafbe358bc855acbc9712224acb0
Noah <83171346+noahlavis@users.noreply.github.com>
2023-11-16 16:13:00 +0100

    Update PowerShellOrchestrator.ps1

--- script/PowerShellOrchestrator.ps1
+++ script/PowerShellOrchestrator.ps1
@@ -1,33 +1,40 @@
 $root = Split-Path -Path $PSScriptRoot -Parent
-$hostname = hostname
-
 $PSOconfig = Get-Content "$root\config.pso"
-
 $maintenance_mode = (($PSOconfig | Select-String -Pattern "maintenance_mode=")-Split "=")[-1]
 $test_mode = (($PSOconfig | Select-String -Pattern "test_mode=")-Split "=")[-1]
 $whitelist = (((($PSOconfig | Select-String -Pattern "whitelist=")-Split "=")[-1]) -Split '"') -Split ","
 
 if($maintenance_mode -eq 0){
 
-    if($whitelist -match $hostname){
+    $hostname = hostname
+    if($whitelist -match $hostname -or $whitelist.Length -eq 1){
 
         $RegRootPath = "HKLM:\SOFTWARE\PowerShellOrchestrator"
         if(Test-Path $RegRootPath){
-        }else{
+        } else{
             New-Item -Path $RegRootPath
         }
 
-        $AllGroups = [regex]::Matches($PSOconfig, '\{(.*?)\}') | ForEach-Object {
-            $_.Groups[1].Value
-        }
+        $AppsRootFolder = Get-ChildItem "$root\apps\"
+        foreach($AppFolder in $AppsRootFolder){
 
-        foreach($Group in $AllGroups){
-            $HostGroup = (((($PSOconfig | Select-String -Pattern "{$Group}") -Split "=")[-1]) -Split '"') -Split ","
-            $HostGroup
+            $AppInfo = Get-Content "$root\apps\$($AppFolder.Name)\instruction.pso"
+            $AppGroup = [regex]::Matches($AppInfo, '\{(.*?)\}') | ForEach-Object {$_.Groups[1].Value}
+            $PSOgroup = (((([regex]::Matches($PSOconfig, "{$AppGroup}")).Value) -Split "{") -Split "}")[1]
+            
+            if($AppGroup -eq $PSOgroup){
+                foreach($lines in $AppInfo){
+                    if($lines.Split(" ")[0] -match "install"){
+                        $install
+                        $install = $($lines.Split(" ")[1])
+                        Start-Process -FilePath "$root\apps\$AppsRootFolder\$install"
+                    }
+                }
+            }
         }
-
-        Set-ItemProperty -Path "HKLM:\SOFTWARE\PowerShellOrchestrator" -Name "GROUPE" -Value $Groupe
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\PowerShellOrchestrator" -Name "GROUP" -Value $Groupe
     }
-}else{
+} else{
     Write-Host "Nothing happen. Maintenance."
 }
+
