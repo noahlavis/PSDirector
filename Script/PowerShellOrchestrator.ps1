ed3f08895f95ac597f9f47394e245c0f4b8f61a8
Noah <83171346+noahlavis@users.noreply.github.com>
2023-11-16 11:22:41 +0100

    Update PowerShellOrchestrator.ps1

--- Script/PowerShellOrchestrator.ps1
+++ Script/PowerShellOrchestrator.ps1
@@ -1,5 +1,5 @@
 $root = Split-Path -Path $PSScriptRoot -Parent
-$hostname
+$hostname = hostname
 
 $PSOconfig = Get-Content "$root\config.pso"
 
@@ -8,15 +8,25 @@ $test_mode = (($PSOconfig | Select-String -Pattern "test_mode=")-Split "=")[-1]
 $whitelist = (((($PSOconfig | Select-String -Pattern "whitelist=")-Split "=")[-1]) -Split '"') -Split ","
 
 if($maintenance_mode -eq 0){
+
     if($whitelist -match $hostname){
+
         $RegRootPath = "HKLM:\SOFTWARE\PowerShellOrchestrator"
         if(Test-Path $RegRootPath){
         }else{
             New-Item -Path $RegRootPath
         }
 
+        $AllGroups = [regex]::Matches($PSOconfig, '\{(.*?)\}') | ForEach-Object {
+            $_.Groups[1].Value
+        }
+
+        foreach($Group in $AllGroups){
+            $HostGroup = (((($PSOconfig | Select-String -Pattern "{$Group}") -Split "=")[-1]) -Split '"') -Split ","
+            $HostGroup
+        }
+
         Set-ItemProperty -Path "HKLM:\SOFTWARE\PowerShellOrchestrator" -Name "GROUPE" -Value $Groupe
-        echo $hostname
     }
 }else{
     Write-Host "Nothing happen. Maintenance."
